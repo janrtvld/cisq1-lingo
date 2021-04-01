9754d5ea819e1b86ded215f9472a295ed21d916b
Jan Rietveld <jan.rietveld@student.hu.nl>
2021-04-01 11:22:43 +0200

    Changed game exception

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Game.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Game.java
@@ -67,7 +67,7 @@ public class Game {
 
     public Round getLatestRound() {
         if(rounds.isEmpty()) {
-            throw new GameStateException(gameStatus);
+            return null;
         }
         return rounds.get(rounds.size() - 1);
     }
